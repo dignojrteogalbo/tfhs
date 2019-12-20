a2a06bc118ac72106bb0ef6b0a406477a70ca523
Digno JR Teogalbo <teogalbo.dj193@hotmail.com>
2019-12-19 19:56:46 -0700

    Build made

---
+++ build/precache-manifest.ddf866c5de53e2062b154edf62498ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866592968844b1ff4ec4dafdd18a5762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d32c5e31957423973d2",
+    "url": "/static/css/main.41a1c344.chunk.css"
+  },
+  {
+    "revision": "7c6b71e6a11a61d70cf6",
+    "url": "/static/js/2.571e202e.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/static/js/2.571e202e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d32c5e31957423973d2",
+    "url": "/static/js/main.6a43fd1b.chunk.js"
+  },
+  {
+    "revision": "48f4b3137a8e692d1b4b",
+    "url": "/static/js/runtime-main.4ee0aff3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
